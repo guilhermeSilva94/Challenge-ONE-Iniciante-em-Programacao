3c14be091ed2667c0d8b8aa4931ebe1091655a45
Guilherme Silva <guime1994@gmail.com>
2023-05-04 18:48:44 -0300

    Adicionando função para criptografar o texto

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,30 @@
 function validarTexto() {
-    const expressaoRegular = /^[a-z]*$/;
+    const expressaoRegular = /^[a-z\s]*$/;
     const validacao = expressaoRegular.test(textoPrincipal.value);
     if (!validacao) {
         aviso.classList.add('aviso-destaque');
     } else {
         aviso.classList.remove('aviso-destaque');
+        criptografar.onclick = criptografarMensagem;
     }
 }
 
+function criptografarMensagem() {
+    let textoCriptografado = textoPrincipal.value;
+    
+    textoCriptografado = textoCriptografado.replace(/e/g, 'enter');
+    textoCriptografado = textoCriptografado.replace(/i/g, 'imes');
+    textoCriptografado = textoCriptografado.replace(/a/g, 'ai');
+    textoCriptografado = textoCriptografado.replace(/o/g, 'ober');
+    textoCriptografado = textoCriptografado.replace(/u/g, 'ufat');
+
+    return alert(textoCriptografado)
+}
+
 const textoPrincipal = document.querySelector('#texto-principal');
-const aviso = document.querySelector('#aviso');    
+const aviso = document.querySelector('#aviso');
+const criptografar = document.querySelector('#criptografar');
+const descriptografar = document.querySelector('#descriptografar');
+const textoCriptografado = ''
 
 textoPrincipal.addEventListener('input', validarTexto);
