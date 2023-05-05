95f25fc71d39f541bcb32f5cb168e869d976a25a
Guilherme Silva <guime1994@gmail.com>
2023-05-05 17:16:10 -0300

    Adicionando função descriptografar e funções para manipular a mensagem

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,28 +3,52 @@ function validarTexto() {
     const validacao = expressaoRegular.test(textoPrincipal.value);
     if (!validacao) {
         aviso.classList.add('aviso-destaque');
-    } else {
+        criptografar.onclick = null;
+        descriptografar.onclick = null;
+    }else {
         aviso.classList.remove('aviso-destaque');
-        criptografar.onclick = criptografarMensagem;
+        criptografar.onclick = mostrarMensagemCriptografada;
+        descriptografar.onclick = mostrarMensagemDescriptografada;
     }
 }
 
-function criptografarMensagem() {
-    let textoCriptografado = textoPrincipal.value;
-    
-    textoCriptografado = textoCriptografado.replace(/e/g, 'enter');
-    textoCriptografado = textoCriptografado.replace(/i/g, 'imes');
-    textoCriptografado = textoCriptografado.replace(/a/g, 'ai');
-    textoCriptografado = textoCriptografado.replace(/o/g, 'ober');
-    textoCriptografado = textoCriptografado.replace(/u/g, 'ufat');
+function criptografarMensagem(texto) {
 
-    return alert(textoCriptografado)
+    texto = texto.replace(/e/g, 'enter');
+    texto = texto.replace(/i/g, 'imes');
+    texto = texto.replace(/a/g, 'ai');
+    texto = texto.replace(/o/g, 'ober');
+    texto = texto.replace(/u/g, 'ufat');
+
+    return texto;
+}
+
+function descriptografarMensagem(texto) {
+
+    texto = texto.replace(/enter/g, 'e');
+    texto = texto.replace(/imes/g, 'i');
+    texto = texto.replace(/ai/g, 'a');
+    texto = texto.replace(/ober/g, 'o');
+    texto = texto.replace(/ufat/g, 'u');
+    
+    return texto;
+}
+
+function mostrarMensagemCriptografada() {
+    const mensagem = textoPrincipal.value;
+    const mensagemCriptografada = criptografarMensagem(mensagem);
+    alert(mensagemCriptografada); 
+}
+
+function mostrarMensagemDescriptografada() {
+    const mensagem = textoPrincipal.value;
+    const mensagemCriptografada = descriptografarMensagem(mensagem);
+    alert(mensagemCriptografada); 
 }
 
 const textoPrincipal = document.querySelector('#texto-principal');
 const aviso = document.querySelector('#aviso');
 const criptografar = document.querySelector('#criptografar');
 const descriptografar = document.querySelector('#descriptografar');
-const textoCriptografado = ''
 
 textoPrincipal.addEventListener('input', validarTexto);
